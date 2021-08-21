a00d340b937ada3bc0a8241a5c8f5b2fe2158e91
Ruslan Skolotiy <dpruslan@gmail.com>
2021-08-21 10:07:27 +0300

    bootstrap

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
 import "./styles.css"
 import { ContextMenu } from "./menu"
 import { BackgroundModule } from "./modules/background.module"
